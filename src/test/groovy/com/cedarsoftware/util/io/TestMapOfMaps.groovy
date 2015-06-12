838ed1d8bbc787e63a99ddabd31e3f6b36594d40
kpartlow <kpartlow@gmail.com>
2015-06-12 11:42:58 -0400

    fixed imports

--- src/test/groovy/com/cedarsoftware/util/io/TestMapOfMaps.groovy
+++ src/test/groovy/com/cedarsoftware/util/io/TestMapOfMaps.groovy
@@ -2,9 +2,14 @@ package com.cedarsoftware.util.io
 
 import org.junit.Test
 
-import java.awt.*
+import java.awt.Point
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotSame
+import static org.junit.Assert.assertNull
+import static org.junit.Assert.assertSame
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.fail
 
 /**
  * Test cases for JsonReader / JsonWriter
