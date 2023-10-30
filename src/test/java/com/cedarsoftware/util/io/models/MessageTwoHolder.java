813ef0777a319a1e3d83b0e1b1323ad6ce9226ac
Ken Partlow <kpartlow@gmail.com>
2023-10-29 21:41:28 -0400

    Situation that cannot be serialized, but should be

---
+++ src/test/java/com/cedarsoftware/util/io/models/MessageTwoHolder.java
@@ -0,0 +1,10 @@
+package com.cedarsoftware.util.io.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+class MessageTwoHolder implements MessageHolder {
+    String message;
+}
