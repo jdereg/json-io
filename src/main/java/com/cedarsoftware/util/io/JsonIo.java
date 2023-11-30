3d0dd718bdf0acfcdf9d7952acdb0b3486afbd1b
John DeRegnaucourt <jdereg@gmail.com>
2023-11-29 21:40:54 -0500

    Going to try bring 5.x ReadOptions/WriteOptions in

--- src/main/java/com/cedarsoftware/util/io/JsonIo.java
+++ src/main/java/com/cedarsoftware/util/io/JsonIo.java
@@ -1,8 +1,44 @@
 package com.cedarsoftware.util.io;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
 /**
- * @author Kenny Partlow (kpartlow@gmail.com)
+ * This is the main API for json-io.  Use these methods to convert:<br/>
+ * <ul>
+ * <li><b>1. Input</b>: Java root | JsonValue root <b>Output</b>: JSON<pre>String json = JsonIo.toJson(JavaObject | JsonValue root, writeOptions)</pre></li>
+ * <li><b>2. Input</b>: Java root | JsonValue root, <b>Output</b>: JSON -> outputStream <pre>JsonIo.toJson(OutputStream, JavaObject | JsonValue root, writeOptions)</pre></li>
+ * <li><b>3. Input</b>: JSON, <b>Output</b>: Java objects<pre>BillingInfo billInfo = JsonIo.toObjects(String | InputStream, readOptions, BillingInfo.class)</pre></li>
+ * <li><b>4. Input</b>: JSON, <b>Output</b>: JsonValues<pre>JsonValue JsonIo.toJsonValues(String | InputStream, readOptions)</pre></li></ul>
+ * Often, the goal is to get JSON to Java objects and from Java objects to JSON.  That is #1 and #3 above. <br/>
+ * <br/>
+ * For approaches #1 and #2 above, json-io will check the root object type (regular Java class or JsonValue instance) to
+ * know which type of Object Graph it is serializing to JSON.<br/>
+ * <br/>
+ * There are occaisons where you may just want the raw JSON data, without anchoring it to a set of "DTO" Java objects.
+ * For example, you may have an extreme amount of data, and you want to process it as fast as possible, and in
+ * streaming mode.  The JSON specification has great primitives which are universally useful in many languages. In
+ * Java that is boolean, null, long [or BigInteger], and double [or BigDecimal], and String.<br/>
+ * <br/>
+ * When JsonValue is returned (option #4 above), your root value will represent one of:
+ * <ul>JSON object {...}<br/>
+ * JSON array [...]<br/>
+ * JSON primitive (boolean, null, long, double, String).</ul>
+ * <br/>
+ * <b>JsonObject</b> implements the JsonValue interface and
+ * represents any JSON object {...}. It also implements the <code> Map</code> interface to make it easy to put/get values to/from it.<br/>
+ * <br/>
+ * <b>JsonArray</b> implements JsonValue and represents an array [...] and also implements<code> List</code> interface, making it
+ * easy to set/get values to/from it.<br/>
+ * <br/>
+ * <b>JsonPrimitive</b> implements JsonValue and represents one of the 5 JSON primitive value types.<br/>
+ * <br/>
+
  * @author John DeRegnaucourt (jdereg@gmail.com)
+ * @author Kenny Partlow (kpartlow@gmail.com)
  *         <br>
  *         Copyright (c) Cedar Software LLC
  *         <br><br>
@@ -19,6 +55,89 @@ package com.cedarsoftware.util.io;
  *         limitations under the License.*
  */
 public class JsonIo {
+    /**
+     * Convert the passed in Java source object to JSON.
+     * @param srcObject Java instance to convert to JSON format.
+     * @param writeOptions Feature options settings to control the JSON output.  Can be null,
+     *                     in which case, default settings will be used.
+     * @return String of JSON that represents the srcObject in JSON format.
+     * @throws JsonIoException A runtime exception thrown if any errors happen during serialization
+     */
+    public static String toJson(Object srcObject, WriteOptions writeOptions) {
+        try {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            JsonWriter writer = new JsonWriter(stream, writeOptions);
+            writer.write(srcObject);
+            writer.close();
+            return stream.toString();
+        } catch (Exception e) {
+            throw new JsonIoException("Unable to convert object to JSON", e);
+        }
+    }
+
+    /**
+     * Convert the passed in Java source object to JSON.  If you want a copy of the JSON that was written to the
+     * OutputStream, you can wrap the output stream before calling this method, like this:<br/>
+     * <br/>
+     * <code>ByteArrayOutputStream baos = new ByteArrayOutputStream(originalOutputStream);<br/>
+     * JsonIo.toJson(baos, source, writeOptions);<br/>
+     * baos.flush();<br/>
+     * String json = new String(baos.toByteArray(), StandardCharsets.UTF_8);<br/>
+     * </code><br/>
+     * @param out OutputStream destination for the JSON output.
+     * @param source Root Java object to begin creating the JSON.
+     * @param writeOptions Feature options settings to control the JSON output.  Can be null,
+     *                     in which case, default settings will be used.
+     * @throws JsonIoException A runtime exception thrown if any errors happen during serialization
+     */
+    public static void toJson(OutputStream out, Object source, WriteOptions writeOptions) {
+        try {
+            JsonWriter writer = new JsonWriter(out, writeOptions);
+            writer.write(source);
+            writer.close();
+        } catch (Exception e) {
+            throw new JsonIoException("Unable to convert object and send in JSON format to OutputStream.", e);
+        }
+    }
+
+    /**
+     * Convert the passed in JSON to Java Objects.
+     * @param json String containing JSON content.
+     * @param readOptions Feature options settings to control the JSON processing.  Can be null,
+     *                     in which case, default settings will be used.
+     * @param rootType Class of the root type of object that will be returned. Can be null, in which
+     *                 case a best-guess will be made for the Class type of the return object.  If it
+     *                 has an @type meta-property that will be used, otherwise the JSON types { ... }
+     *                 will return a Map, [...] will return Object[] or Collection, and the primtive
+     *                 types will be returned (String, long, Double, boolean, or null).
+     * @return rootType Java instance that represents the Java equivalent of the passed in JSON string.
+     * @throws JsonIoException A runtime exception thrown if any errors happen during serialization
+     */
+    public static <T> T toObjects(String json, ReadOptions readOptions, Class<T> rootType) {
+        if (json == null) {
+            return null;
+        }
+        return toObjects(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)), readOptions, rootType);
+    }
+
+    /**
+     * Convert the passed in JSON to Java Objects.
+     * @param in InputStream bringing JSON content.
+     * @param readOptions Feature options settings to control the JSON processing.  Can be null,
+     *                     in which case, default settings will be used.
+     * @param rootType Class of the root type of object that will be returned. Can be null, in which
+     *                 case a best-guess will be made for the Class type of the return object.  If it
+     *                 has an @type meta-property that will be used, otherwise the JSON types { ... }
+     *                 will return a Map, [...] will return Object[] or Collection, and the primtive
+     *                 types will be returned (String, long, double, boolean, or null).
+     * @return rootType Java instance that represents the Java equivalent of the JSON input.
+     * @throws JsonIoException A runtime exception thrown if any errors happen during serialization
+     */
+    public static <T> T toObjects(InputStream in, ReadOptions readOptions, Class<T> rootType) {
+        try (JsonReader jr = new JsonReader(in, readOptions)) {
+            return (T) jr.readObject(rootType);
+        }
+    }
 
     public static String formatJson(String json, ReadOptions readOptions, WriteOptions writeOptions) {
         if (writeOptions.isBuilt())
@@ -34,13 +153,20 @@ public class JsonIo {
                 new ReadOptionsBuilder().returnAsMaps().build(),
                 new WriteOptions().prettyPrint(true));
     }
-
-    public static <T> T deepCopy(Object o) {
-        return deepCopy(o, new ReadOptionsBuilder().build(), new WriteOptions());
-    }
-
-    public static <T> T deepCopy(Object o, ReadOptions readOptions, WriteOptions writeOptions) {
-        String json = JsonWriter.toJson(o, writeOptions);
-        return JsonReader.toObjects(json, readOptions, null);
+    
+     /**
+     * Copy an object graph using JSON.
+     * @param source Object root object to copy
+     * @param readOptions ReadOptions feature settings. Can be null for default ReadOptions.
+     * @param writeOptions WriteOptions feature settings. Can be null for default WriteOptions.
+     * @return A new, duplicate instance of the original.
+     */
+    public static <T> T deepCopy(Object source, ReadOptions readOptions, WriteOptions writeOptions) {
+        if (source == null) {
+            // They asked to copy null.  The copy of null is null.
+            return null;
+        }
+        String json = toJson(source, writeOptions);
+        return (T) toObjects(json, readOptions, source.getClass());
     }
 }
